8364b05983c1f620011372b071cf9a89707268c0
PLekraie <pauline.lekraie@gmail.com>
2022-04-13 16:52:10 +0200

    feat(bonsai): add bonsai

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function switchView(viewName) {
-  $(".viewsElement").addClass("hidden");
+	$('.viewsElement').addClass('hidden');
 
-  $(`#${viewName}`).removeClass('hidden');
+	$(`#${viewName}`).removeClass('hidden');
+
+	if ($('#menu').hasClass('opened')) changeMenu();
+}
+
+function changeMenu() {
+	if (!$('#menu').hasClass('opened')) {
+		$('#menu').addClass('opened');
+		$('#burger').empty().append('<i class="fa-solid fa-xmark"></i>');
+	} else {
+		$('#menu').removeClass('opened');
+		$('#burger').empty().append('<i class="fa-solid fa-burger"></i>');
+	}
 }
